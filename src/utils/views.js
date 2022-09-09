484160bb21aadd1250bad24c96d52ecdfbec15c2
eliza <elizabet002@mail.ru>
2022-09-09 15:30:23 +0300

    removed an extra class of an modal element

--- src/utils/views.js
+++ src/utils/views.js
@@ -88,7 +88,7 @@ export const renderPosts = (posts, elements, i18n) => {
     const li = document.createElement('li');
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
     const a = document.createElement('a');
-    a.classList.add('fw-bold', 'link-secondary');
+    a.classList.add('fw-bold');
     a.dataset.id = post.id;
     a.setAttribute('href', post.link);
     a.setAttribute('target', '_blank');
